68ab2d256ee77cc5530f1e205d423229978ae577
EmilyJaynee <emily.barrott@hotmail.co.uk>
2024-09-03 21:41:38 +0100

    Stremlined api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ function updateTemp(responce) {
   console.log(responce.data.temperature.current);
   let liveTemperature = document.querySelector("#current-temp");
   let temperature = responce.data.temperature.current;
+  let cityName = document.querySelector("#cityName");
+
+  cityName.innerHTML = responce.data.city;
   liveTemperature.innerHTML = Math.round(temperature);
 }
 
@@ -15,10 +18,11 @@ function retrieveInfo(city) {
 function searchCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#search");
-  let cityName = document.querySelector("#cityName");
-  cityName.innerHTML = `${cityInput.value}`;
+
   retrieveInfo(cityInput.value);
 }
 
 let search = document.querySelector("#current-city-forms");
 search.addEventListener("submit", searchCity);
+
+retrieveInfo("Brighton");
