495e795296e85b5c9186339a371f60afb70def22
EmilyJaynee <emily.barrott@hotmail.co.uk>
2024-09-06 00:04:11 +0100

    Changed time to AM / PM

--- src/index.js
+++ src/index.js
@@ -4,13 +4,25 @@ function updateTemp(responce) {
   let liveTemperature = document.querySelector("#current-temp");
   let temperature = responce.data.temperature.current;
   let weatherCondions = document.querySelector("#condiontons");
+  let time = document.querySelector("#currentTime");
+
+  let date = new Date(responce.data.time * 1000);
 
   cityName.innerHTML = responce.data.city;
   countryName.innerHTML = responce.data.country;
   liveTemperature.innerHTML = Math.round(temperature);
   weatherCondions.innerHTML = responce.data.condition.description;
+  time.innerHTML = formatTime(date);
 }
+function formatTime(date) {
+  let amPm = {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  };
 
+  return date.toLocaleTimeString("en-US", amPm);
+}
 function retrieveInfo(city) {
   let apiKey = "19884f8731abea4oebtff3a019e58351";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
@@ -21,7 +33,6 @@ function retrieveInfo(city) {
 function searchCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#search");
-
   retrieveInfo(cityInput.value);
 }
 
